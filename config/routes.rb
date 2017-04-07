4fc19df4d7a86e43d5ce43e2b0803dd5d94b8b28
Collin <c_orear@icloud.com>
2017-04-07 19:52:39 +0000

    Big Update. Base set.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+  devise_for :users, controllers: { confirmations: 'confirmations' }
   devise_scope :user do
   get 'sign_in', to: 'devise/sessions#new'
   get 'sign_up', to: 'devise/registrations#new'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170405214523) do
+ActiveRecord::Schema.define(version: 20170406204404) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 20170405214523) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "username"
+    t.string   "firstName"
+    t.string   "lastName"
+    t.string   "gender"
+    t.string   "denomination"
+    t.string   "city"
+    t.string   "state"
+    t.string   "interests"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
